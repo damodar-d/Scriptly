4b82251889093cd24f5f22cc9c0514849525b7bd
damodar-d <abhiram.deshpande00@gmail.com>
2026-06-27 16:10:51 +0530

    install-cuda ubuntu-26.04 version13.3

---
+++ install-cuda-toolkit.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+set -e
+
+#Installation Location for CUDA -> /usr/local
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2604/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda-toolkit-13-3
